8ae210727ab3c3ddd35a8de6fe2967bf9015068b
Florijn <wim.florijn@kadaster.nl>
2020-06-22 11:18:38 +0200

    working sync

--- bridge/src/app.module.ts
+++ bridge/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { SensorQueryModule } from './sync/sensor/sensor.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SensorQueryModule } from './sync/sensor/sensor.module';
 
 const port = process.env.MONGO_PORT || 27017;
 const host = process.env.MONGO_HOST || 'localhost';
--- bridge/src/sync/sensor/sensor.module.ts
+++ bridge/src/sync/sensor/sensor.module.ts
@@ -1,14 +1,16 @@
-import { Logger, Module, OnModuleInit } from '@nestjs/common';
-import { EventStoreModule } from '../../event-store/event-store.module';
-import { EventStorePublisher } from '../../event-store/event-store.publisher';
-import { SensorController } from './sensor.controller';
-import { SensorProcessor } from './processors';
 import { CqrsModule } from '@nestjs/cqrs';
-import { LedgerInterface } from './ledger-interface.service';
-import { RetrieveSensorsQueryHandler } from './queries/sensors.handler';
+import { SensorProcessor } from './processors';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StateSchema } from './models/state.model';
+import { SensorController } from './sensor.controller';
+import { LedgerInterface } from './ledger-interface.service';
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { TCPWriteEventsOptions } from 'geteventstore-promise';
+import { EventStoreModule } from '../../event-store/event-store.module';
+import { RetrieveSensorsQueryHandler } from './queries/sensors.handler';
+import { EventStorePublisher } from '../../event-store/event-store.publisher';
+
+const NODE_ID = process.env.NODE_ID || 'node_1';
 
 @Module({
   imports: [
@@ -38,27 +40,30 @@ export class SensorQueryModule implements OnModuleInit {
 
   onModuleInit() {
     const onEvent = (_, eventMessage) => {
-      this.sensorProcessor.process(eventMessage);
+      if (eventMessage.data.nodeId == NODE_ID) {
+        this.sensorProcessor.process(eventMessage);
+      }
     };
 
     this.eventStore.subscribeToStream('$ce-sensor', onEvent, () => {
-      this.logger.warn('Event stream dropped');
+      this.logger.warn('Event stream dropped.');
     });
 
     const publishEventCallback = (event) => {
-      const newEvent = {
-        streamId: `test-${event.aggregateId}`,
-        eventType: event.eventType,
-        data: event,
-      };
+      if (event.nodeId != NODE_ID) {
+        const newEvent = {
+          streamId: `sensor-${event.aggregateId}`,
+          eventType: event.eventType,
+          data: event,
+        };
 
-      const writeEventOptions: TCPWriteEventsOptions = {
-        expectedVersion: event.eventNumber > 0 ? event.eventNumber - 1 : -1,
-      };
+        const writeEventOptions: TCPWriteEventsOptions = {
+          expectedVersion: event.eventNumber - 1,
+        };
 
-      this.eventStore.createEvent(newEvent, writeEventOptions)
-        .then(() => {
-        }, (error) => this.logger.log(`Event has already been written: ${error.toString()}`));
+        this.eventStore.createEvent(newEvent, writeEventOptions)
+            .then(() => {}, (error) => this.logger.log(`Event has already been written: ${error.toString()}`));
+      }
     };
     this.ledgerInterface.initContractListener(publishEventCallback);
   }
