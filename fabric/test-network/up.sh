b777f34bf04ee08ebb4263dec360c3d2e1103c66
Florijn <wim.florijn@kadaster.nl>
2020-06-23 13:47:28 +0200

    Update scripts to add new org.

---
+++ fabric/test-network/up.sh
@@ -0,0 +1,126 @@
+VERSION=1
+IMAGETAG=2.1.1
+CA_IMAGETAG=1.4.7
+
+MAX_RETRY=5
+CLI_DELAY=3
+VERBOSE=false
+CC_SRC_LANGUAGE=typescript
+
+DATABASE="goleveldb"
+CHANNEL_NAME="mychannel"
+CRYPTO="Certificate Authorities"
+
+COMPOSE_FILE_BASE=docker/docker-compose-test-net.yaml
+COMPOSE_FILE_COUCH=docker/docker-compose-couch.yaml
+COMPOSE_FILE_CA=docker/docker-compose-ca.yaml
+
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=${PWD}/configtx
+
+function createOrgs() {
+  if [ -d "organizations/peerOrganizations" ]; then
+    rm -Rf organizations/peerOrganizations && rm -Rf organizations/ordererOrganizations
+  fi
+
+  if [ "$CRYPTO" == "Certificate Authorities" ]; then
+
+    echo
+    echo "##########################################################"
+    echo "##### Generate certificates using Fabric CA's ############"
+    echo "##########################################################"
+
+    IMAGE_TAG=${CA_IMAGETAG} docker-compose -f $COMPOSE_FILE_CA up -d 2>&1
+
+    . organizations/fabric-ca/registerEnroll.sh
+
+    sleep 10
+
+    echo "##########################################################"
+    echo "############ Create Org1 Identities ######################"
+    echo "##########################################################"
+
+    createOrg1
+
+    echo "##########################################################"
+    echo "############ Create Org2 Identities ######################"
+    echo "##########################################################"
+
+    createOrg2
+
+    echo "##########################################################"
+    echo "############ Create Orderer Org Identities ###############"
+    echo "##########################################################"
+
+    createOrderer
+
+  fi
+
+  echo
+  echo "Generate CCP files for Org1 and Org2"
+  ./organizations/ccp-generate.sh
+}
+
+function createConsortium() {
+  which configtxgen
+  if [ "$?" -ne 0 ]; then
+    echo "configtxgen tool not found. exiting"
+    exit 1
+  fi
+
+  echo "#########  Generating Orderer Genesis block ##############"
+
+  set -x
+  configtxgen -profile TwoOrgsOrdererGenesis -channelID system-channel -outputBlock ./system-genesis-block/genesis.block
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate orderer genesis block..."
+    exit 1
+  fi
+}
+
+function networkUp() {
+
+  createOrgs
+  createConsortium
+
+  COMPOSE_FILES="-f ${COMPOSE_FILE_BASE}"
+  if [ "${DATABASE}" == "couchdb" ]; then
+    COMPOSE_FILES="${COMPOSE_FILES} -f ${COMPOSE_FILE_COUCH}"
+  fi
+
+  IMAGE_TAG=$IMAGETAG docker-compose ${COMPOSE_FILES} up -d 2>&1
+
+  docker ps -a
+  if [ $? -ne 0 ]; then
+    echo "ERROR !!!! Unable to start network"
+    exit 1
+  fi
+
+}
+
+function createChannel() {
+
+ scripts/createChannel.sh $CHANNEL_NAME $CLI_DELAY $MAX_RETRY $VERBOSE
+  if [ $? -ne 0 ]; then
+    echo "Error !!! Create channel failed"
+    exit 1
+  fi
+
+}
+
+function deployCC() {
+
+  scripts/deployCC.sh $CHANNEL_NAME $CC_SRC_LANGUAGE $VERSION $CLI_DELAY $MAX_RETRY $VERBOSE
+
+  if [ $? -ne 0 ]; then
+    echo "ERROR !!! Deploying chaincode failed"
+    exit 1
+  fi
+
+}
+
+networkUp
+createChannel
+deployCC
