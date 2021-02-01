3925cd43b3df637eacd623e4ea11201ba6aee1c9
Wim Florijn <wim.florijn@kadaster.nl>
2021-02-01 19:22:26 +0100

    fix dups

--- src/sync/core/events/sensor/registered/1.0.0/sensor-registered.event.ts
+++ src/sync/core/events/sensor/registered/1.0.0/sensor-registered.event.ts
@@ -8,53 +8,22 @@ export class SensorRegistered extends SensorEvent {
 
   public readonly version = SensorRegistered.version;
 
-  @Expose()
-  readonly organizationId: string;
-
-  @Expose()
-  readonly name: string;
-
-  @Expose()
-  readonly longitude: number;
-
-  @Expose()
-  readonly latitude: number;
-
-  @Expose()
-  readonly height: number;
-
-  @Expose()
-  readonly baseObjectId: string;
-
-  @Expose()
-  readonly aim: string;
-
-  @Expose()
-  readonly description: string;
-
-  @Expose()
-  readonly manufacturer: string;
-
-  @Expose()
-  readonly active: boolean;
-
-  @Expose()
-  readonly observationArea: Record<string, any>;
-
-  @Expose()
-  readonly documentationUrl: string;
-
-  @Expose()
-  readonly theme: string[];
-
-  @Expose()
-  readonly category: string;
-
-  @Expose()
-  readonly typeName: string;
-
-  @Expose()
-  readonly typeDetails: Record<string, any>;
+  @Expose() readonly organizationId: string;
+  @Expose() readonly name: string;
+  @Expose() readonly longitude: number;
+  @Expose() readonly latitude: number;
+  @Expose() readonly height: number;
+  @Expose() readonly baseObjectId: string;
+  @Expose() readonly aim: string;
+  @Expose() readonly description: string;
+  @Expose() readonly manufacturer: string;
+  @Expose() readonly active: boolean;
+  @Expose() readonly observationArea: Record<string, any>;
+  @Expose() readonly documentationUrl: string;
+  @Expose() readonly theme: string[];
+  @Expose() readonly category: string;
+  @Expose() readonly typeName: string;
+  @Expose() readonly typeDetails: Record<string, any>;
 
   constructor(sensorId: string, organizationId: string,
               name: string, longitude: number, latitude: number, height: number,
