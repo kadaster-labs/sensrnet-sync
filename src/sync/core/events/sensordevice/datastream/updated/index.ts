2d6d0949a45a0e53c0419fd4a9dbf1b4a1177ed7
marcvanandel <marc.vanandel@kadaster.nl>
2021-03-26 10:58:05 +0100

    fixed filenaming pattern

--- src/sync/core/events/sensordevice/datastream/updated/index.ts
+++ src/sync/core/events/sensordevice/datastream/updated/index.ts
@@ -1,8 +1,8 @@
 import { plainToClass } from 'class-transformer';
 import { EventMessage } from '../../../event-message';
-import { DatastreamUpdated as V1 } from './datastreamupdated-v1.event';
+import { DatastreamUpdated as V1 } from './datastream-updated-v1.event';
 
-export { DatastreamUpdated } from './datastreamupdated-v1.event';
+export { DatastreamUpdated } from './datastream-updated-v1.event';
 
 export function getDatastreamUpdatedEvent(eventMessage: EventMessage): V1 {
     return !eventMessage.metadata.version || eventMessage.metadata.version === V1.version ? plainToClass(V1, eventMessage.data) : null;
